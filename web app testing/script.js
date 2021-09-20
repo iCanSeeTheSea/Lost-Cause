07fde8c658e8eaadeef965f5197740fa746ce3ff
hellomeister3 <44944360+hellomeister3@users.noreply.github.com>
2021-09-20 19:01:06 +0100

    adding stuff

--- web app testing/script.js
+++ web app testing/script.js
@@ -3,14 +3,14 @@ var myGamePiece;
 
 function startGame() {
     myGameArea.start();
-    myGamePiece = new component(30, 30, "red", 10, 120);
+    myGamePiece = new component(10, 10, "red", 2, 2);
 }
 
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-        this.canvas.width = 750;
-        this.canvas.height = 750;
+        this.canvas.width = 500;
+        this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
@@ -19,7 +19,7 @@ var myGameArea = {
             myGameArea.keys[event.keyCode] = (event.type == "keydown");
         })
         window.addEventListener('keyup', function (event) {
-            myGameArea.keys[event.keyCode] = (e.type == "keydown");            
+            myGameArea.keys[event.keyCode] = (event.type == "keydown");            
         })
     }, 
     clear : function(){
@@ -46,15 +46,14 @@ function component(width, height, color, x, y) {
     }    
 }
 
-
 function updateGameArea() {
     myGameArea.clear();
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
-    if (myGameArea.keys && myGameArea.keys == 87) {myGamePiece.speedY = -1; } // w
-    if (myGameArea.keys && myGameArea.keys == 65) {myGamePiece.speedX = -1; } // a
-    if (myGameArea.keys && myGameArea.keys == 83) {myGamePiece.speedY = 1; } // s
-    if (myGameArea.keys && myGameArea.keys == 68) {myGamePiece.speedX = 1; } // d
+    if (myGameArea.keys && myGameArea.keys[87] && !(myGamePiece.y <= 0)) {myGamePiece.speedY = -2; } // w
+    if (myGameArea.keys && myGameArea.keys[65] && !(myGamePiece.x <= 0)) {myGamePiece.speedX = -2; } // a
+    if (myGameArea.keys && myGameArea.keys[83] && !(myGamePiece.y >= myGameArea.canvas.height - 10)) {myGamePiece.speedY = 2; } // s
+    if (myGameArea.keys && myGameArea.keys[68] && !(myGamePiece.x >= myGameArea.canvas.height - 10)) {myGamePiece.speedX = 2; } // d
     myGamePiece.newPos();
     myGamePiece.update();
 }
