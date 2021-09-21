7d2710cb1aea5d7b36c82042667c9ee3685334be
hellomeister3 <hellomeister3@gmail.com>
2021-09-21 10:20:37 +0100

    preliminary obstacles

--- web app testing/script.js
+++ web app testing/script.js
@@ -1,5 +1,6 @@
 
 var myGamePiece;
+var myObstacles = [];
 
 function startGame() {
     myGameArea.start();
@@ -9,32 +10,32 @@ function startGame() {
 var myGameArea = {
     canvas : document.createElement("canvas"),
     start : function() {
-<<<<<<< HEAD
         this.canvas.width = 500;
-=======
-        this.canvas.width = 480;
->>>>>>> 4d4efbe25e3dda38055ec8325c63ebc38ece0f91
         this.canvas.height = 500;
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.frameNo = 0;        
         this.interval = setInterval(updateGameArea, 20);
         window.addEventListener('keydown', function (event) {
             myGameArea.keys = (myGameArea.keys || []);
             myGameArea.keys[event.keyCode] = true;
-            console.log(event.keyCode)
-            console.log(myGameArea.keys)
         })
         window.addEventListener('keyup', function (event) {
-<<<<<<< HEAD
             myGameArea.keys[event.keyCode] = (event.type == "keydown");            
-=======
-            myGameArea.keys[event.keyCode] = false;            
->>>>>>> 4d4efbe25e3dda38055ec8325c63ebc38ece0f91
         })
     }, 
     clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    },
+    stop : function(){
+        clearInterval(this.interval)
+        console.log("stopped")
     }
+}
+
+function everyinterval(n){
+    if ((myGameArea.frameNo/n)%1 == 0) {return true;}
+    return false;
 }
 
 function component(width, height, color, x, y) {
@@ -53,25 +54,57 @@ function component(width, height, color, x, y) {
     this.newPos = function() {
         this.x += this.speedX;
         this.y += this.speedY;        
+    }
+    this.crashWith = function(otherobj) {
+        var myleft = this.x;
+        var myright = this.x + (this.width);
+        var mytop = this.y;
+        var mybottom = this.y + (this.height);
+        var otherleft = otherobj.x;
+        var otherright = otherobj.x + (otherobj.width);
+        var othertop = otherobj.y;
+        var otherbottom = otherobj.y + (otherobj.height);
+        var crash = false;
+        if ((mybottom > otherbottom) ||
+        (mytop < othertop) ||
+        (myright > otherright) ||
+        (myleft < otherleft)) {
+          crash = true;
+        }
+        return crash;
     }    
 }
 
 function updateGameArea() {
     myGameArea.clear();
+    var x, y;
+    for (i = 0; i < myObstacles.length; i += 1) {
+      if (myGamePiece.crashWith(myObstacles[i])) {
+        myGameArea.stop();
+        return;
+      }
+    }
+    myGameArea.clear();
+    myGameArea.frameNo += 1;
+    if (myGameArea.frameNo == 1 || everyinterval(150)) {
+      x = 0;
+      y = 0;
+      myObstacles.push(new component(50, 50, "white", x, y));
+    }
+    for (i = 0; i < myObstacles.length; i += 1) {
+      myObstacles[i].update();
+    }
+
     myGamePiece.speedX = 0;
     myGamePiece.speedY = 0;
-<<<<<<< HEAD
-    if (myGameArea.keys && myGameArea.keys[87] && !(myGamePiece.y <= 0)) {myGamePiece.speedY = -2; } // w
-    if (myGameArea.keys && myGameArea.keys[65] && !(myGamePiece.x <= 0)) {myGamePiece.speedX = -2; } // a
-    if (myGameArea.keys && myGameArea.keys[83] && !(myGamePiece.y >= myGameArea.canvas.height - 10)) {myGamePiece.speedY = 2; } // s
-    if (myGameArea.keys && myGameArea.keys[68] && !(myGamePiece.x >= myGameArea.canvas.height - 10)) {myGamePiece.speedX = 2; } // d
-=======
-    if (myGameArea.keys && myGameArea.keys == 87) {myGamePiece.speedY = -1; } // w
-    if (myGameArea.keys && myGameArea.keys == 65) {myGamePiece.speedX = -1; } // a
-    if (myGameArea.keys && myGameArea.keys == 83) {myGamePiece.speedY = 1; } // s
-    if (myGameArea.keys && myGameArea.keys == 68) {myGamePiece.speedX = 1; } // d
-    console.log(myGamePiece.speedX, myGamePiece.speedY)
->>>>>>> 4d4efbe25e3dda38055ec8325c63ebc38ece0f91
+    if (myGameArea.keys && myGameArea.keys[87] && 
+        !(myGamePiece.y <= 0)) {myGamePiece.speedY = -2; } // w
+    if (myGameArea.keys && myGameArea.keys[65] && 
+        !(myGamePiece.x <= 0)) {myGamePiece.speedX = -2; } // a
+    if (myGameArea.keys && myGameArea.keys[83] && 
+        !(myGamePiece.y >= myGameArea.canvas.height - 10)) {myGamePiece.speedY = 2; } // s
+    if (myGameArea.keys && myGameArea.keys[68] && 
+        !(myGamePiece.x >= myGameArea.canvas.height - 10)) {myGamePiece.speedX = 2; } // d
     myGamePiece.newPos();
     myGamePiece.update();
 }
