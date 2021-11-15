bf69df86df7e2c6752238bf8960c2a216fcaa0a9
hellomeister3 <44944360+hellomeister3@users.noreply.github.com>
2021-11-15 19:32:43 +0000

    smoothing the camera following

--- 2D camera following/script.js
+++ 2D camera following/script.js
@@ -8,37 +8,40 @@ var held_directions = []; //State of which arrow keys we are holding down
 var speed = 1; //How fast the character moves in pixels per frame
 
 const placeCharacter = () => {
-   
+
    var pixelSize = parseInt(
       getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
    );
-   
+
    const held_direction = held_directions[0];
+   console.log(held_directions)
+   console.log(held_direction)
    if (held_direction) {
-      if (held_direction === directions.right) {x += speed;}
-      if (held_direction === directions.left) {x -= speed;}
-      if (held_direction === directions.down) {y += speed;}
-      if (held_direction === directions.up) {y -= speed;}
+      if (held_direction === directions.right) { x += speed; }
+      if (held_direction === directions.left) { x -= speed; }
+      if (held_direction === directions.down) { y += speed; }
+      if (held_direction === directions.up) { y -= speed; }
       character.setAttribute("facing", held_direction);
+      character.setAttribute("walking", held_direction ? "true" : "false");
    }
-   character.setAttribute("walking", held_direction ? "true" : "false");
-   
+   // character.setAttribute("walking", held_direction ? "true" : "false");
+
    //Limits (gives the illusion of walls)
    var leftLimit = -8;
-   var rightLimit = (16 * 11)+8;
+   var rightLimit = (16 * 11) + 8;
    var topLimit = -8 + 32;
    var bottomLimit = (16 * 7);
    if (x < leftLimit) { x = leftLimit; }
    if (x > rightLimit) { x = rightLimit; }
    if (y < topLimit) { y = topLimit; }
    if (y > bottomLimit) { y = bottomLimit; }
-   
-   
+
+
    var camera_left = pixelSize * 66;
    var camera_top = pixelSize * 42;
-   
-   map.style.transform = `translate3d( ${-x*pixelSize+camera_left}px, ${-y*pixelSize+camera_top}px, 0 )`;
-   character.style.transform = `translate3d( ${x*pixelSize}px, ${y*pixelSize}px, 0 )`;  
+
+   map.style.transform = `translate3d( ${-x * pixelSize + camera_left}px, ${-y * pixelSize + camera_top}px, 0 )`;
+   character.style.transform = `translate3d( ${x * pixelSize}px, ${y * pixelSize}px, 0 )`;
 }
 
 
--- webapp/app/static/js/app.js
+++ webapp/app/static/js/app.js
@@ -1,5 +1,84 @@
-console.log('hello from app.js!')
 
-function displayMaze(spanningTree){
-    console.log(spanningTree)
+function displayMaze(spanningTree) {
+    //     console.log(spanningTree)
 }
+
+let held_directions = [];
+let character = document.querySelector('.character');
+var map = document.querySelector(".map");
+let x = 15;
+let y = 15;
+let speed = 1;
+
+const placeCharacter = function () {
+    var pixelSize = parseInt(
+        getComputedStyle(document.documentElement).getPropertyValue('--pixel-size')
+    );
+    const held_direction = held_directions[0];
+    if (held_direction) {
+        if (held_direction === directions.right) { x += speed; }
+        if (held_direction === directions.left) { x -= speed; }
+        if (held_direction === directions.down) { y += speed; }
+        if (held_direction === directions.up) { y -= speed; }
+        character.setAttribute("facing", held_direction);
+        character.setAttribute("walking", held_direction ? "true" : "false");
+    }
+
+    let charX = x;
+    let charY = y;
+    let mapX = x;
+    let mapY = y;
+    let mapMulti = 10;
+
+    if (charX < 0 + 5) { charX = 0 + 5; } // left
+    if (charX > 15.5 * mapMulti - 5) { charX = 15.5 * mapMulti - 5; } // right
+    if (charY < 0 + 5) { charY = 0 + 5; } // top
+    if (charY > 15.5 * mapMulti - 5) { charY = 15.5 * mapMulti - 5; } // bottom
+
+    if (mapX < 6.2 * mapMulti) { mapX = 6.2 * mapMulti; } // left
+    if (mapX > 7.8 * mapMulti) { mapX = 7.8 * mapMulti; } // right
+    if (mapY < 6.2 * mapMulti) { mapY = 6.2 * mapMulti; } // top
+    if (mapY > 7.8 * mapMulti) { mapY = 7.8 * mapMulti; } // bottom
+    var camera_top = pixelSize * mapMulti * 6.2;
+    let camera_left = pixelSize * mapMulti * 6.2;
+    map.style.transform = `translate3d( ${-mapX * pixelSize + camera_left}px, ${-mapY * pixelSize + camera_top}px, 0 )`;
+    character.style.transform = `translate3d( ${charX * pixelSize}px, ${charY * pixelSize}px, 0 )`;
+
+}
+
+const step = function () {
+    placeCharacter()
+    window.requestAnimationFrame(function () {
+        step()
+    })
+}
+step()
+
+
+const directions = {
+    up: "up",
+    down: "down",
+    left: "left",
+    right: "right",
+}
+const keys = {
+    'w': directions.up,
+    'a': directions.left,
+    'd': directions.right,
+    's': directions.down,
+}
+
+document.addEventListener('keydown', function (e) {
+    let dir = keys[e.key];
+    if (dir && held_directions.indexOf(dir) === -1) {
+        held_directions.unshift(dir);
+    }
+})
+
+document.addEventListener('keyup', function (e) {
+    let dir = keys[e.key];
+    let index = held_directions.indexOf(dir);
+    if (index > -1) {
+        held_directions.splice(index, 1)
+    }
+})
