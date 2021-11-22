82ef56a4b73a299ec5166ce4c60b6f16dd4e18dd
ceres17 <hellomeister3@gmail.cp>
2021-11-22 15:26:21 +0000

    trying to make the pictures work

--- webapp/app/drawMaze.py
+++ webapp/app/drawMaze.py
@@ -1,7 +1,13 @@
 from PIL import Image
+from pathlib import Path
+
+# mazePath = Path('static/img/maze/')
 
 width = 400
 height = 300
-img = Image.new(mode='RGB', size=(width, height), color=(0, 16, 41))
+#img = Image.new(mode='RGB', size=(width, height), color=(0, 16, 41))
+
+# base = mazePath / 'base.png'
+img = Image.open('base.png')
 
 img.show()
