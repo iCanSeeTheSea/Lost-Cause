8610e7b9942d549b1e8146652aed72891e8f224a
ceres17 <hellomeister3@gmail.cp>
2021-12-16 09:46:47 +0000

    the maze drawing works!

--- webapp/app/drawMaze.py
+++ webapp/app/drawMaze.py
@@ -2,7 +2,7 @@ from PIL import Image
 from pathlib import Path
 import generateMaze
 
-SIDELEN = 5
+SIDELEN = 40
 
 spanning_tree = generateMaze.mazeGen(SIDELEN,SIDELEN)
 
@@ -17,12 +17,15 @@ img = Image.open(base)
 tile = mazePath / 'no-walls.png'
 tileImg = Image.open(tile)
 
-img = img.resize((SIDELEN*32*2,SIDELEN*32*2))
+img = img.resize((SIDELEN*32*2-32,SIDELEN*32*2-32))
+# tiles are 32x32
 
 for node in spanning_tree:
-    img.paste(tileImg, (node[0][0]-1*32, node[0][1]-1*32))
-    # join_x = ((node[1][0] - node[0][0])/2) + node[0][0] 
-    # join_y = ((node[1][1] - node[0][1])/2) + node[0][1]
-    # img.paste(tileImg, (join_x-1*32, join_y-1*32))
+    adjNodes = spanning_tree[node]
+    img.paste(tileImg, ((node[0]-1)*64, (node[1]-1)*64))
+    for adjNode in adjNodes:
+        join_x = ((adjNode[0] - node[0])/2) + node[0] 
+        join_y = ((adjNode[1] - node[1])/2) + node[1]
+        img.paste(tileImg, (int((join_x-1)*64), int((join_y-1)*64)))
 
 img.show()
