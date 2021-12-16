1afb8653b98747e9a8258baa7270d572da2d2d6f
ceres17 <hellomeister3@gmail.cp>
2021-12-16 10:57:22 +0000

    adding the maze picture to the web app

--- webapp/app/drawMaze.py
+++ webapp/app/drawMaze.py
@@ -1,31 +1,27 @@
 from PIL import Image
 from pathlib import Path
-import generateMaze
 
-SIDELEN = 40
+def mazeImgGen(SIDELEN, spanning_tree):
+    mazePath = Path('static/img/maze/')
 
-spanning_tree = generateMaze.mazeGen(SIDELEN,SIDELEN)
+    width = 400
+    height = 300
 
-mazePath = Path('static/img/maze/')
+    base = mazePath / 'base.png'
+    img = Image.open(base)
 
-width = 400
-height = 300
+    tile = mazePath / 'no-walls.png'
+    tileImg = Image.open(tile)
 
-base = mazePath / 'base.png'
-img = Image.open(base)
+    img = img.resize((SIDELEN*32*2-32,SIDELEN*32*2-32))
+    # tiles are 32x32
 
-tile = mazePath / 'no-walls.png'
-tileImg = Image.open(tile)
+    for node in spanning_tree:
+        adjNodes = spanning_tree[node]
+        img.paste(tileImg, ((node[0]-1)*64, (node[1]-1)*64))
+        for adjNode in adjNodes:
+            join_x = ((adjNode[0] - node[0])/2) + node[0] 
+            join_y = ((adjNode[1] - node[1])/2) + node[1]
+            img.paste(tileImg, (int((join_x-1)*64), int((join_y-1)*64)))
 
-img = img.resize((SIDELEN*32*2-32,SIDELEN*32*2-32))
-# tiles are 32x32
-
-for node in spanning_tree:
-    adjNodes = spanning_tree[node]
-    img.paste(tileImg, ((node[0]-1)*64, (node[1]-1)*64))
-    for adjNode in adjNodes:
-        join_x = ((adjNode[0] - node[0])/2) + node[0] 
-        join_y = ((adjNode[1] - node[1])/2) + node[1]
-        img.paste(tileImg, (int((join_x-1)*64), int((join_y-1)*64)))
-
-img.show()
+    img.save(mazePath / "fullmaze.png")
