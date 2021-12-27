dd55c130b176e15992a3f81ce03cf3e24afe78ee
hellomeister3 <44944360+hellomeister3@users.noreply.github.com>
2021-12-27 13:07:45 +0000

    fixed drawMaze

--- webapp/app/drawMaze.py
+++ webapp/app/drawMaze.py
@@ -1,8 +1,9 @@
 from PIL import Image
 from pathlib import Path
 
+
 def mazeImgGen(SIDELEN, spanning_tree):
-    mazePath = Path('static/img/maze/')
+    mazePath = Path('app/static/img/maze/')
 
     width = 400
     height = 300
@@ -13,16 +14,15 @@ def mazeImgGen(SIDELEN, spanning_tree):
     tile = mazePath / 'no-walls.png'
     tileImg = Image.open(tile)
 
-    img = img.resize((SIDELEN*32*2-32,SIDELEN*32*2-32))
+    img = img.resize((SIDELEN*32*2-32, SIDELEN*32*2-32))
     # tiles are 32x32
 
     for node in spanning_tree:
         adjNodes = spanning_tree[node]
         img.paste(tileImg, ((node[0]-1)*64, (node[1]-1)*64))
         for adjNode in adjNodes:
-            join_x = ((adjNode[0] - node[0])/2) + node[0] 
+            join_x = ((adjNode[0] - node[0])/2) + node[0]
             join_y = ((adjNode[1] - node[1])/2) + node[1]
             img.paste(tileImg, (int((join_x-1)*64), int((join_y-1)*64)))
-
 
     img.save(mazePath / "fullmaze.png")
--- webapp/app/views.py
+++ webapp/app/views.py
@@ -1,14 +1,16 @@
 from app import app
 from flask import render_template
 from app import generateMaze
-#from app import drawMaze
+from app import drawMaze
+
 
 @app.route('/')
 def index():
     return render_template('public/index.html')
 
+
 @app.route('/play')
 def about():
-    spanning_tree = generateMaze.mazeGen(20,20)
-    #drawMaze.mazeImgGen(20, spanning_tree)
+    spanning_tree = generateMaze.mazeGen(20, 20)
+    drawMaze.mazeImgGen(20, spanning_tree)
     return render_template('public/play.html', spanningTree=spanning_tree)
