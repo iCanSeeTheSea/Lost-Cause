4b3b4790e96f6a42027fa1166be411dc31e8d328
sea <44944360+ceres17@users.noreply.github.com>
2022-02-28 12:37:42 +0000

    adding primms to the main game

--- webapp/app/views.py
+++ webapp/app/views.py
@@ -9,7 +9,7 @@ def index():
 
 @app.route('/play')
 def about():
-    map_size = 10
-    spanning_tree = generateMaze.mazeGen(map_size, map_size)
+    map_size = 20
+    spanning_tree = generateMaze.primms(map_size, map_size)
     spanning_tree = drawMaze.mazeImgGen(map_size, spanning_tree)
     return render_template('public/play.html', spanningTree=spanning_tree, mapSize=map_size)
